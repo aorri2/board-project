55f49727c1883bc570c06e1529aa2767c67edd0c
aorri2 <aorri96@naver.com>
2022-12-19 22:16:58 +0900

    build: [#4] 프로젝트 생성
    Closes #4

---
+++ src/main/java/com/wuk/boardprj/BoardPrjApplication.java
@@ -0,0 +1,13 @@
+package com.wuk.boardprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wuk/boardprj/BoardPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuk.boardprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
